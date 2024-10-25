38436da789264d71c7b05af4f78f371233b74b8c
psymore <egeeozel@gmail.com>
2024-10-25 23:56:30 +0300

    Typo fix.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/rosuter';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MagicSchoolModule } from './D&D/magic-school/magic-school.module';
 
